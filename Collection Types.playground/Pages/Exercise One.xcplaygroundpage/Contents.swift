388deae238f6cf7c44295c9e2a488b21256f798e
Salvarajah, Jheeven <jsalvarajah@lcs-mba3020.lcs.on.ca>
2018-02-13 19:20:23 -0500

    Finished Excersie 1

--- Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
+++ Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
@@ -8,22 +8,36 @@
  */
 
 import Foundation
-
+var grades : [Double] = []
 
 /*:
  2. Now populate the array with eight random grades between 60 and 100.
  */
-
+for _ in 1...5 {
+    grades.append( Double( arc4random_uniform(90) + 10 ))
+}
 
 /*:
  3. Print a list of each array index and the associated value.
  */
-
+print("There are \(grades.count) values in the array right now.")
 
 /*:
  4. Determine and print the highest grade.
  */
+var highest = 80.0       // Keeps track of the lowest value found so far
 
+// Inspect each temperature one by one
+for grade in grades {
+    
+    // Is this temperature less than the lowest so far?
+    if grade > highest {
+        highest = grade // If so, replace the lowest so far with this temperature
+    }
+}
+
+// The lowest temperature is...
+print("The lowest temperature is \(highest) degrees Celsius.")
 
 //: [Next](@next)
 
